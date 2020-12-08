114b8abc6ffc6edbf9b174cfaf1bd61c42094cd4
cbandera1 <cbandera@utem.cl>
2020-12-08 00:43:46 -0300

    Comienzo diseño web

---
+++ WebContent/javascript/utils.js
@@ -0,0 +1,10 @@
+$(document).on("click", "#somebutton", function() {  // When HTML DOM "click" event is invoked on element with ID "somebutton", execute the following function...
+    $.get("Enlistador", function(responseJson) {    // Execute Ajax GET request on URL of "someservlet" and execute the following function with Ajax response JSON...
+        var $ul = $("<ul>").appendTo($("#somediv")); // Create HTML <ul> element and append it to HTML DOM element with ID "somediv".
+        $.each(responseJson, function(index, item) { // Iterate over the JSON array.
+        	var string = "(" + item.Tipo + ") ID: " + item.ID +
+        					" en (" + item.posX + ", " + item.posY + ")";
+            $("<li>").text(string).appendTo($ul);      // Create HTML <li> element, set its text content with currently iterated item and append it to the <ul>.
+        });
+    });
+});
