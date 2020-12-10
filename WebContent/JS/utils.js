cf51bb9fb755e96e8bbeff335442371c8556f742
cbandera1 <cbandera@utem.cl>
2020-12-10 19:05:58 -0300

    Avance pagina asignarPuntosdeVenta

--- WebContent/JS/utils.js
+++ WebContent/JS/utils.js
@@ -16,16 +16,50 @@ $(document).one("click", "#somebutton1", () => {
 			for (var i = 0; i < data.length; i++) {
 				const obj = data[i];
 				var mainContainer = document.getElementById("detail");
-				if (obj.Tipo == "P") {
+				if (obj.Tipo == "C") {
 					var header = document.createElement("div");
-					header.innerHTML = '<div class= "font-italic"> (' + obj.Tipo +') ID: ' + obj.ID + ' Pos: (' + obj.posX + ', ' + obj.posY + '): ' 
-										+ '<input name="' + obj.ID + '" type="number" value="0"></div>' 
+					header.innerHTML = '<div class= "font-italic"> (' + obj.Tipo + ') ID: ' + obj.ID + ' Pos: (' + obj.posX + ', ' + obj.posY + '): '
+						+ '<input name="' + obj.ID + '" type="number" value="0"></div>'
 					mainContainer.appendChild(header);
 				}
 			}
 		})
 		.catch((error) => { console.log(error); });
 });
+
+$(document).one("click", "#somebutton2", () => {
+	fetch('Enlistador')
+		.then((response) => { return response.json(); })
+		.then((data) => {
+			for (var j = 0; j < data.length; j++) {
+				const obj = data[j];
+				var mainContainer = document.getElementById("detail");
+				var sel = document.createElement("select");
+				if (obj.Tipo == "P") {
+					for (var i = 0; i < data.length; i++) {
+						const obj1 = data[i];
+						var mainContainer = document.getElementById("detail");
+						if (obj1.Tipo == "C") {
+							var opt = document.createElement('OPTION');
+							opt.setAttribute('name', obj.ID);
+							opt.innerHTML = '(' + obj1.Tipo + ') ID: ' + obj1.ID + ' Pos: (' + obj1.posX + ', ' + obj1.posY + ')';
+							sel.appendChild(opt);
+						}
+					}
+					var header = document.createElement("div");
+					header.innerHTML = '<div class= "font-italic"> (' + obj.Tipo + ') ID: ' + obj.ID + ' Pos: (' + obj.posX + ', ' + obj.posY + ')</div>'
+					mainContainer.appendChild(header);
+					mainContainer.appendChild(sel);
+					
+				}
+			}
+		})
+		.catch((error) => { console.log(error); });
+});
+
+
+
+
 
 
 
